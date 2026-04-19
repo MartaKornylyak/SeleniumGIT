5391589a94fb6ec4e23d2108e8bbdef9fd93c966
MartaKornylyak <128946109+MartaKornylyak@users.noreply.github.com>
2026-04-19 13:58:17 -0500

    Remove print statement for 'Added from GIT'

--- src/classwork/HotelSelenium.java
+++ src/classwork/HotelSelenium.java
@@ -5,8 +5,7 @@ public class HotelSelenium {
 	public static void main(String[] args) {
 		System.out.println("Hotel Selenium");
 		System.out.println("Added locally");
-		System.out.println("Added from GIT");
-
+		
 
 	}
 
