091e930454ff0f30abdb2126ecb7a9f5acee37a6
MartaKornylyak <128946109+MartaKornylyak@users.noreply.github.com>
2026-04-19 10:18:39 -0500

    Add evening greeting to HelloWorld

--- src/classwork/HelloWorld.java
+++ src/classwork/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world good moring");
+		System.out.println("Hello world good everning");
+
 	}
 
 }
