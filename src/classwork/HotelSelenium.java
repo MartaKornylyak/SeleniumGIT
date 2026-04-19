8bfbd73bd25a498f494f8863c5467142b33bc990
marta <marta@Vega.lan>
2026-04-19 11:29:51 -0500

    commit5

--- src/classwork/HotelSelenium.java
+++ src/classwork/HotelSelenium.java
@@ -7,10 +7,7 @@ public class HotelSelenium {
 		System.out.println("Added locally");
 		System.out.println("Added from GIT");
 		System.out.println("Added from GIT"); 
-		System.out.println("Added locally2");
-		
-
-	}
+		}
 
 	}
 
