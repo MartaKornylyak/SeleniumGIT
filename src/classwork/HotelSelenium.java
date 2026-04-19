f2693a163b24eebde319bb3fd5045860a9e99515
MartaKornylyak <128946109+MartaKornylyak@users.noreply.github.com>
2026-04-19 11:58:06 -0500

    Add additional print statement in HotelSelenium

--- src/classwork/HotelSelenium.java
+++ src/classwork/HotelSelenium.java
@@ -6,6 +6,7 @@ public class HotelSelenium {
 		System.out.println("Hotel Selenium");
 		System.out.println("Added locally");
 		System.out.println("Added from GIT");
+		System.out.println("Added from GIT2");
 
 	}
 
