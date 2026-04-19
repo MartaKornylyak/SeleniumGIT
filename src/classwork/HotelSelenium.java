c5b29b0cab910e7306c11459a20cbd60e5c245f4
MartaKornylyak <128946109+MartaKornylyak@users.noreply.github.com>
2026-04-19 11:57:21 -0500

    Fix merge conflict in HotelSelenium.java
    Removed merge conflict markers and updated print statements.

--- src/classwork/HotelSelenium.java
+++ src/classwork/HotelSelenium.java
@@ -6,13 +6,7 @@ public class HotelSelenium {
 		System.out.println("Hotel Selenium");
 		System.out.println("Added locally");
 		System.out.println("Added from GIT");
-<<<<<<< HEAD
-		System.out.println("Added from GIT"); 
-		
-=======
-		System.out.println("Added locally2");
->>>>>>> branch 'master' of https://github.com/MartaKornylyak/SeleniumGIT.git
-		
+
 	}
 
 	}
