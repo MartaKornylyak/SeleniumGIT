da7bde5544ebc9f7bb65388d32c5196d37572753
MartaKornylyak <128946109+MartaKornylyak@users.noreply.github.com>
2026-04-19 10:20:39 -0500

    Fix typos in HelloWorld output messages

--- src/classwork/HelloWorld.java
+++ src/classwork/HelloWorld.java
@@ -3,8 +3,10 @@ package classwork;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world good moring");
+		System.out.println("Hello world good morning");
 		System.out.println("Hello world good everning");
+		System.out.println("Hello world good afternoon");
+
 
 	}
 
